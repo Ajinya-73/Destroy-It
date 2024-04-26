951943e222b8083f17acd4e55d0be44704b92a0a
Ajinkya793 <ajinkya.gaikwad73@gmail.com>
2024-04-26 15:30:28 +0530

    AI Test

---
+++ Assets/scripts/Enemy AI Scripts/EnemyAI2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI2 : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] wayPoints;
+    int wayPointIndex;
+    Vector3 target;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWayPointIndex();
+            UpdateDestination();
+        }
+    }
+
+    void UpdateDestination()
+    {
+        target = wayPoints[wayPointIndex].position;
+        agent.SetDestination(target);
+    }
+
+    void IterateWayPointIndex()
+    {
+        wayPointIndex++;
+
+        if (wayPointIndex == wayPoints.Length)
+        {
+            wayPointIndex = 0;
+        }
+
+    }
+
+}
