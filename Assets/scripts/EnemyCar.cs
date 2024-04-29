0785fba3351c756079faa047523e3cac6f72c62d
Ajinkya793 <ajinkya.gaikwad73@gmail.com>
2024-04-29 17:08:18 +0530

    Enemy AI Car test

--- Assets/scripts/EnemyCar.cs
+++ Assets/scripts/EnemyCar.cs
@@ -3,44 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyCar : MonoBehaviour
 {
     NavMeshAgent agent;
     public Transform[] wayPoints;
     int wayPointIndex;
     Vector3 target;
-
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         UpdateDestination();
     }
 
-
+    
     void Update()
     {
-        if (Vector3.Distance(transform.position, target) < 1)
+        if (Vector3.Distance(transform.position,target) < 1)
         {
             IterateWayPointIndex();
             UpdateDestination();
         }
+        
     }
+    void IterateWayPointIndex()
+    {
+        wayPointIndex++;
 
+        if (wayPointIndex == wayPoints.Length)
+        {
+            wayPointIndex = 0;
+        }
+        
+    }
     void UpdateDestination()
     {
         target = wayPoints[wayPointIndex].position;
         agent.SetDestination(target);
     }
 
-    void IterateWayPointIndex()
-    {
-        wayPointIndex ++;
+    
 
-        if (wayPointIndex == wayPoints.Length)
-        {
-            wayPointIndex = 0;
-        }
-
-    }
 
 }
